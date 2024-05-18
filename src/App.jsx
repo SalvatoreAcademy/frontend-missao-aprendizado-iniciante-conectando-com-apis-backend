7c79b2b9889ffd6c5d519b7343473e342112f79f
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-18 22:41:38 +0000

    Tratamento de erros com `react-toastify`

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const [devmons, setDevmons] = useState([])
 
   async function fetchData() {
-    const apiUrl = 'https://backend-iniciante-integrar-com-frontend.onrender.com/personagem'
+    const apiUrl = 'https://backend-iniciante-integrar-com-frontend.onrender.com/personagemm'
 
-    const response = await fetch(apiUrl)
+    const response = await fetch(apiUrl).catch(function (error) {
+      console.error('Erro ao chamar endpoint /personagem', error)
+      toast.error('Erro ao carregar lista de DevMon')
+    })
 
-    const data = await response.json()
+    if (response.ok) {
+      const data = await response.json()
 
-    setDevmons(data)
+      setDevmons(data)
+    } else {
+      toast.error('Erro ao carregar lista de DevMon')
+    }
   }
 
   useEffect(function () {
@@ -26,6 +35,7 @@ function App() {
           return <Card key={devmon.nome} item={devmon} />
         })}
       </div>
+      <ToastContainer />
     </>
   )
 }
