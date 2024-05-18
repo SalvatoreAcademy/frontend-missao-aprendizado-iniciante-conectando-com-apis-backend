ae0643e70f1fa44d6e39e243880f951ad5d04d42
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-18 22:17:08 +0000

    Carregamento de dados via API com Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,19 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const java = {
-    nome: 'Java',
-    imagem: 'https://salvatore.academy/devmon/1_java.png',
-    evoluiPara: 'Kotlin'
+  const devmons = []
+
+  async function fetchData() {
+    const apiUrl = 'https://backend-iniciante-integrar-com-frontend.onrender.com/personagem'
+
+    const response = await fetch(apiUrl)
+
+    const data = await response.json()
+
+    console.log(45, data)
   }
 
-  const kotlin = {
-    nome: 'Kotlin',
-    imagem: 'https://salvatore.academy/devmon/2_kotlin.png',
-    evoluiPara: 'Android'
-  }
-
-  const android = {
-    nome: 'Android',
-    imagem: 'https://salvatore.academy/devmon/3_android.png'
-  }
-
-  const c = {
-    nome: 'C',
-    imagem: 'https://salvatore.academy/devmon/4_c.png',
-    evoluiPara: 'C++'
-  }
-
-  const cplusplus = {
-    nome: 'C++',
-    imagem: 'https://salvatore.academy/devmon/5_cpp.png',
-    evoluiPara: 'C#'
-  }
-
-  const devmons = [java, kotlin, android, c, cplusplus]
+  fetchData()
 
   return (
     <>
